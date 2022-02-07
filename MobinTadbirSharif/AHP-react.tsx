290e732ba3fb4ed8b346dc6c9ecc3df7ef7c40c1
amirporia99.1378@gmail.com <amirporia99.1378@gmail.com>
2022-02-07 09:17:35 +0330

    npm-package

---
+++ MobinTadbirSharif/AHP-react.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const AHPReact = () => {
+    return (
+        <button>
+            <div>
+                mobin tadbir make our life better ?
+            </div>
+        </button>
+    );
+}
+
+export default AHPReact;
